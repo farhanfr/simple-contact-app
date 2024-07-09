296871f1a537b1189667d9bff7c38dc1362c5ceb
Mochamad Farhan Fitrahtur Rachmad <farhanfitrahtur@gmail.com>
2024-07-09 15:41:42 +0700

    feat: add schema of simple unit testing

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -2,16 +2,38 @@
  * @format
  */
 
-import 'react-native';
+// import 'react-native';
+// import React from 'react';
+// import App from '../App';
+
+// // Note: import explicitly to use the types shipped with jest.
+import {it,expect,describe} from '@jest/globals';
+
+// // Note: test renderer must be required after react-native.
+// import renderer from 'react-test-renderer';
+
+// it('renders correctly', () => {
+//   renderer.create(<App />);
+// });
+
+
 import React from 'react';
-import App from '../App';
+import { render } from '@testing-library/react-native';
+import ListContactScreen from '../src/screens/list_contact';
 
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('ListContactScreen renders correctly', () => {
+  const { getByText } = render(<ListContactScreen />);
+  const textElement = getByText('List Contacts');
+  expect(textElement).toBeTruthy();
 });
+
+
+describe('ListContactScreen check function', () => {
+  it('should navigate to DetailContactScreen when press item', () => {
+    const { getByText } = render(<ListContactScreen />);
+    const textElement = getByText('List Contacts');
+    fireEvent.press(textElement);
+    expect(navigation.navigate).toHaveBeenCalledWith(DETAIL_CONTACT_ROUTE, { id: '1' });
+  });
+});
+
